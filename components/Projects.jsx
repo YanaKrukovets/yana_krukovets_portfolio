4c2894adc2b3aa2af75a14a3967321e0a780374c
Yana Krukovets <yanashelli@gmail.com>
2022-12-07 13:52:58 -0500

    Add new projects

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -12,7 +12,9 @@ const Projects = () => {
             <Project href="https://memorygame.rf.gd" src="/images/memory.jpg" alt="Memory game website" tech="JavaScript, HTML, CSS"/>
             <Project href="https://meal-iota.vercel.app/" src="/images/meal.jpg" alt="Meal search website" tech="Vue.js, Vuex, Vite, Tailwind.css"/>
             <Project href="https://reactfilm.vercel.app/" src="/images/movie.jpg" alt="Movie search website" tech="ReactJS, Tailwind.css"/>
+            <Project href="https://google-search-ashy.vercel.app/search" src="/images/Google.jpg" alt="Search website" tech="ReactJS, Tailwind.css, Rapid API"/>
             <Project href="https://superquiz.great-site.net" src="/images/quiz.jpg" alt="Quiz website" tech="JavaScript, HTML, CSS"/>
+            <Project href="https://blog-next-graph-ql.vercel.app/" src="/images/blog.jpg" alt="Search website" tech="ReactJS, Next.js, GRaphQL, Hygraph cms"/>
             <Project href="https://doughnut.epizy.com" src="/images/donuts.jpg" alt="Doughnuts website" tech="JavaScript, PHP, CSS, HTML, Bootstrap"/>
         </div>
         <p>Also I was a part of a big team who was working on the &quot;Dentrix Ascend&quot; system (Dental Software)</p>
